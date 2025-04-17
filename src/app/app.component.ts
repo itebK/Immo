96e3bc680ff912e15d121125487de51fd276ebc8
wissemkhayati <khayati.wissem@gmail.com>
2025-04-17 14:07:37 -0400

    updates
    * create component property
    * create component 404notfound
    * routes -> create paths + edit app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./pages/home/home.component";
-import { PropertyCardComponent } from './shared/components/property-card/property-card.component';
 import { NavbarComponent } from './shared/components/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent,NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PagenotfoundComponent } from './pages/404notfound/pagenotfound.component';
+import { PropertyComponent } from './pages/property/property.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'landingPage',
+    component: HomeComponent,
+  },
+  { path: ':property', component: PropertyComponent },
+  // This should always be the last route
+  { path: '**', component: PagenotfoundComponent }
+];
