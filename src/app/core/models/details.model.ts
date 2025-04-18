3d6e15ed84dcac68b40f8756e4362dd1ed07a404
wissemkhayati <khayati.wissem@gmail.com>
2025-04-17 22:33:09 -0400

    add icons

--- src/app/core/models/details.model.ts
+++ src/app/core/models/details.model.ts
@@ -1,9 +1,11 @@
 export class Details {
   name: string;
   value: any;
+  icon: any;
 
   constructor(data: any) {
     this.value = data.value;
     this.name = data.name;
+    this.icon = data.icon;
   }
 }
