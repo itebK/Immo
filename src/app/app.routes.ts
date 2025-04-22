6dcf474b55dd4734727e9d13cb0ad90837a97f03
iteb khayati <khayati.iteb@gmail.com>
2025-04-22 12:50:41 -0400

    Lazy loading pour tous les composants

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,16 @@
 import { Routes } from '@angular/router';
-import { PropertyDetailComponent } from './features/properties/property-detail/property-detail.component';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
-import { HomeComponent } from './features/home/home.component';
-import { PagenotfoundComponent } from './features/pagenotfound/pagenotfound.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
     children: [
-      { path: '', component: HomeComponent },
-      { path: 'landingPage', component: HomeComponent },
-      { path: 'details/:id', component: PropertyDetailComponent },
-      { path: '**', component: PagenotfoundComponent },
+      { path: '', loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent) },
+      { path: 'landingPage', loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent) },
+      { path: 'details/:id', loadComponent: () => import('./features/properties/property-detail/property-detail.component').then(m => m.PropertyDetailComponent) },
+      { path: '**', loadComponent: () => import('./features/pagenotfound/pagenotfound.component').then(m => m.PagenotfoundComponent) },
     ]
   },
   {
@@ -24,3 +21,5 @@ export const routes: Routes = [
     ]
   }
 ];
+
+
