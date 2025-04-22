6860a9064130e6f0fe69983fae5e091ce170335a
wissemkhayati <khayati.wissem@gmail.com>
2025-04-22 17:58:25 -0400

    format document

--- src/app/shared/components/property-card/property-card.component.ts
+++ src/app/shared/components/property-card/property-card.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Property } from '../../../core/models/property/property.model';
 import { MatIconModule } from '@angular/material/icon';
+import { PropertyType } from '../../../core/models/enums/property-type.enum';
 
 @Component({
   selector: 'app-property-card',
@@ -10,14 +11,15 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [CommonModule, MatIconModule],
   templateUrl: './property-card.component.html',
   styleUrls: ['./property-card.component.scss'],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA] 
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class PropertyCardComponent {
+  readonly propertyType = PropertyType;
   @Input() data!: Property;
   formatType(type: string, mode: string): string {
-    const capitalizedType = type.charAt(0).toUpperCase() + type.slice(1).toLowerCase();
+    const capitalizedType =
+      type.charAt(0).toUpperCase() + type.slice(1).toLowerCase();
     const modeLabel = mode === 'location' ? 'à louer' : 'à vendre';
     return `${capitalizedType} ${modeLabel}`;
   }
-  
 }
